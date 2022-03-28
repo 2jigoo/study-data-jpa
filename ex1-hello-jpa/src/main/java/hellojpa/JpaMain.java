46b41da57a40ea3b2db44b9bf5f2ee5b01708dfe
2jigoo <2jigoo@naver.com>
2022-03-28 23:53:30 +0900

    2. 영속성 컨텍스트

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,17 +18,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("이맘바");
+//            selectList(em);
+//            insertAndCache(em);
 
-            List<Member> members = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+            Member findMember1 = em.find(Member.class, 100L);
+            Member findMember2 = em.find(Member.class, 100L);
 
-            for (Member member : members) {
-                System.out.println("member.name: " + member.getName());
-            }
+            System.out.println("findMember == findMember2 = " + (findMember1 == findMember2));
 
             tx.commit();
         } catch (Exception e) {
@@ -38,5 +34,34 @@ public class JpaMain {
         }
 
         emf.close(); // application 종료시
+    }
+
+    private static void insertAndCache(EntityManager em) {
+        Member member = new Member();
+        member.setId(100L);
+        member.setName("Hello JPA");
+
+        System.out.println("=== BEFORE ===");
+        em.persist(member);
+        System.out.println("=== AFTER ===");
+
+        Member findMember = em.find(Member.class, 100L);
+
+        System.out.println("findMember.id = " + findMember.getId());
+        System.out.println("findMember.name = " + findMember.getName());
+    }
+
+    private static void selectList(EntityManager em) {
+        Member findMember = em.find(Member.class, 1L);
+        findMember.setName("이맘바");
+
+        List<Member> members = em.createQuery("select m from Member as m", Member.class)
+                .setFirstResult(5)
+                .setMaxResults(8)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.println("member.name: " + member.getName());
+        }
     }
 }
