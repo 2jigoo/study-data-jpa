b64f151271f0635e6f253f25235c4b73017faf3f
2jigoo <2jigoo@naver.com>
2022-03-30 00:12:35 +0900

    3. 플러시, 준영속 상태

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,11 +20,15 @@ public class JpaMain {
         try {
 //            selectList(em);
 //            insertAndCache(em);
+//            equalsWhenPkIsSame(em);
+//            sendSQLWhenCommit(em);
 
-            Member findMember1 = em.find(Member.class, 100L);
-            Member findMember2 = em.find(Member.class, 100L);
+            Member member = em.find(Member.class, 150L);
+            member.setName("이멤버");
+            // 1. em.persist() 해야할 것 같지만, 엔티티의 변경이 감지되어 update 쿼리가 나간다. (dirty check)
 
-            System.out.println("findMember == findMember2 = " + (findMember1 == findMember2));
+            em.detach(member);
+            // 2. 준영속 상태로 만들면, 영속성 컨텍스트에서 관리되지 않으므로 변경감지가 되지 않는다.
 
             tx.commit();
         } catch (Exception e) {
@@ -34,6 +38,25 @@ public class JpaMain {
         }
 
         emf.close(); // application 종료시
+    }
+
+    private static void sendSQLWhenCommit(EntityManager em) {
+        Member member1 = new Member(150L, "A");
+        Member member2 = new Member(160L, "B");
+
+        em.persist(member1);
+        em.persist(member2);
+
+        System.out.println("============================");
+        // ===== 이후에 쿼리가 찍힌다
+        // 영속성 컨텍스트에 엔티티와 쿼리를 쌓아두다가 커밋하는 시점에 DB에 쿼리 보냄.
+    }
+
+    private static void equalsWhenPkIsSame(EntityManager em) {
+        Member findMember1 = em.find(Member.class, 100L);
+        Member findMember2 = em.find(Member.class, 100L);
+
+        System.out.println("findMember == findMember2 = " + (findMember1 == findMember2));
     }
 
     private static void insertAndCache(EntityManager em) {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -14,4 +14,11 @@ public class Member {
     private Long id;
     private String name;
 
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
