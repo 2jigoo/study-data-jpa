52c9b94b06a022b4bf2cdea23a7e1cfbffe4be70
2jigoo <2jigoo@naver.com>
2022-11-09 13:33:49 +0900

    2. 쿼리 메소드 - (3) @Query

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,12 +2,18 @@ package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    // 2-1. 메소드명
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    // 2-3. @Query
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 
 }
