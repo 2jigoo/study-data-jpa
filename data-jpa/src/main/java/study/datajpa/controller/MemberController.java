55d64d52b905dedaac56c95c0e41ed5fa55a5eae
2jigoo <2jigoo@naver.com>
2023-02-28 21:06:28 +0900

    3. 확장 기능 - (4) 페이징과 정렬

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,15 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDTO;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -30,9 +36,34 @@ public class MemberController {
         return member.getUsername();
     }
 
+    /**
+     * {@code spring.data.web.pageable.one-indexed-parameters}를 {@code true}로 설정해도
+     * index가 0부터 시작하는 문제가 있음.
+     * Custom PageRequest, Page를 만들어 변환하는 방법 권장.
+     * <pre>
+     * {@code
+     * PageRequest request = PageRequest.of(1, 10);
+     * Page<Member> page = memberRepository.findAll(request).map(MemberDTO::of);
+     * MyPage<Member> myPage = MyPage.of(page);
+     * return myPage;
+     * }
+     * </pre>
+     */
+    @GetMapping("/members")
+    public Page<MemberDTO> list(
+            @PageableDefault(size = 20, sort = "username", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        // 직접 엔티티를 반환해선 안 된다.
+        return memberRepository.findAll(pageable)
+                .map(MemberDTO::of);
+    }
+
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 
 }
